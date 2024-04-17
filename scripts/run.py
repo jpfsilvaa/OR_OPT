3e9b6ed4d4ecdcd65088a02478383d32e4c0925b
jpfsilvaa <joaopaulofsilvaa@gmail.com>
2024-04-17 10:38:42 -0300

    _init__.py for importing packages

---
+++ scripts/run.py
@@ -0,0 +1,17 @@
+import multiprocessing
+import subprocess
+import os
+
+def run_script(scriptPath):
+    scriptPath.insert(0, 'python')
+    subprocess.run(scriptPath)
+
+if __name__ == '__main__':
+    home = os.path.expanduser("~")
+    pathToModel = os.path.join(home, 'github', 'OR_OPT', 'models','hc_model_gurobi.py')
+    
+    scriptCalls = [
+        [pathToModel, 'instances/INST_1/']
+    ]
+
+    #todo: FINISH THIS
