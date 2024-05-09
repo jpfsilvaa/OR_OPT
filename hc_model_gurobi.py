c66dde7e015ff3190c46c7eb60eca977245ea060
jpfsilvaa <joaopaulofsilvaa@gmail.com>
2024-05-09 14:58:44 -0300

    constraint for the two CCA rooms that work only in the morning

--- hc_model_gurobi.py
+++ hc_model_gurobi.py
@@ -6,10 +6,10 @@ from classes.parameters import ModelParameters
 def getParameters():
     params = ModelParameters()
 
-    df_infra = pd.read_csv('instances/INST_4/infra_salas(psi).csv', sep=',')
+    df_infra = pd.read_csv('instances/INST_2/infra_salas(psi).csv', sep=',')
     params.operRooms = df_infra.columns[1:].tolist()
 
-    df_espec = pd.read_csv('instances/INST_4/especialidades.csv', sep=',')
+    df_espec = pd.read_csv('instances/INST_2/especialidades.csv', sep=',')
     params.specialties = df_espec['Sub-especialidades'].tolist()
     params.demand = {s: df_espec.loc[df_espec['Sub-especialidades'] == s, 'Demanda'].values[0] for s in params.specialties}
     params.priority = {s: df_espec.loc[df_espec['Sub-especialidades'] == s, 'Prioridade'].values[0] for s in params.specialties}
@@ -23,7 +23,7 @@ def getParameters():
             infra[i][k] = df_infra.loc[df_infra['Sub-especialidade/sala'] == k, i].values[0]
     params.infra = infra
     
-    df_disp_times = pd.read_csv('instances/INST_4/disp_times(lambda).csv', sep=',')
+    df_disp_times = pd.read_csv('instances/INST_2/disp_times(lambda).csv', sep=',')
     blockWeekIds = df_disp_times['bloco_semana_id'].tolist()
     
     teamsAvailab = {}
@@ -33,7 +33,7 @@ def getParameters():
             teamsAvailab[k][j] = df_disp_times.loc[df_disp_times['bloco_semana_id'] == j, k].values[0]
     params.teamsAvailab = teamsAvailab
     
-    df_disp_anest = pd.read_csv('instances/INST_4/disp_anestesista(A).csv', sep=',')
+    df_disp_anest = pd.read_csv('instances/INST_2/disp_anestesista(A).csv', sep=',')
     params.blockIds = df_disp_anest['bloco_id'].tolist()[:-1]
 
     anestAvailab = {}
@@ -57,6 +57,7 @@ def createModel(params):
     m.addConstrs((gp.quicksum(x.sum('*', b, s) for b in params.blockIds) <= (1 + params.ubDemand) * params.demand[s] for s in params.specialties), name='UpperBoundDemand')
     m.addConstrs((x.sum(o, b, '*') <= 1 for o in params.operRooms for b in params.blockIds), name='BlockSpecialty')
     m.addConstrs((x[o, b, s] == 0 for o in params.operRooms for b in params.blockIds for s in params.specialties if params.infra[o][s] == 0), name='Infrastructure')
+    m.addConstrs((x[o, b, s] == 0 for o in params.operRooms[-2:] for i,b in enumerate(params.blockIds) for s in params.specialties if i % 2 == 0), name='CCAWorkPeriod')
     m.addConstrs((gp.quicksum(params.needAnest[s] * x.sum('*', b, s) for s in params.specialties) <= params.anestAvailab[b] for b in params.blockIds), name='Anesthetists')
     m.addConstrs((x.sum('*', b, s) <= params.teamsAvailab[s][b[:3]] for b in params.blockIds for s in params.specialties), name='SpecialtyPerBlock')
 
