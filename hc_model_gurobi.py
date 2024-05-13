6fad01e4d4ec7003b5a1f40fb4c8d74e3a3f7e51
jpfsilvaa <joaopaulofsilvaa@gmail.com>
2024-05-13 12:20:53 -0300

    testing constraints and variables to define a perfect scenario about human resources (anesthesists and teams availability)

--- hc_model_gurobi.py
+++ hc_model_gurobi.py
@@ -54,6 +54,8 @@ def createModel(params):
 
     x = m.addVars(params.operRooms, params.blockIds, params.specialties, vtype=gp.GRB.BINARY, name="x")
     z = m.addVars(params.specialties, vtype=gp.GRB.INTEGER, name="z")
+    h = m.addVars(params.blockIds, params.specialties, vtype=gp.GRB.INTEGER, name="H")
+    a = m.addVars(params.blockIds, vtype=gp.GRB.INTEGER, name="A")
 
     m.addConstrs((gp.quicksum(x.sum('*', b, s) for b in params.blockIds) <= (1 + params.ubDemand) * params.demand[s] for s in params.specialties), name='UpperBoundDemand')
     m.addConstrs((x.sum(o, b, '*') <= 1 for o in params.operRooms for b in params.blockIds), name='BlockSpecialty')
@@ -61,6 +63,8 @@ def createModel(params):
     m.addConstrs((x.sum(o, b, '*') == 0 for o in params.operRooms[-2:] for i,b in enumerate(params.blockIds) if i % 2 != 0), name='CCAWorkPeriod')
     m.addConstrs((gp.quicksum(params.needAnest[s] * x.sum('*', b, s) for s in params.specialties) <= params.anestAvailab[b] for b in params.blockIds), name='Anesthetists')
     m.addConstrs((x.sum('*', b, s) <= params.teamsAvailab[s][b[:3]] for b in params.blockIds for s in params.specialties), name='SpecialtyPerBlock')
+    # m.addConstrs((gp.quicksum(params.needAnest[s] * x.sum('*', b, s) for s in params.specialties) <= a[b] for b in params.blockIds), name='Anesthetists')
+    # m.addConstrs((x.sum('*', b, s) <= h[b, s] for b in params.blockIds for s in params.specialties), name='SpecialtyPerBlock')
 
     if params.modelType == 'M1':
         m.addConstrs((gp.quicksum(x.sum('*', b, s) for b in params.blockIds) >= params.demand[s] for s in params.specialties), name='SpecialtyDemand')
@@ -69,13 +73,15 @@ def createModel(params):
         m.addConstrs((gp.quicksum(x.sum('*', b, s) for b in params.blockIds) + z[s] >= params.demand[s] for s in params.specialties), name='SpecialtyDemand')
         m.setObjective(gp.quicksum(params.priority[s] * (params.revenue[s] - params.cost[s]) * x[o, b, s] for o in params.operRooms for b in params.blockIds for s in params.specialties) - gp.quicksum(params.revenue[s]* z[s] for s in params.specialties), gp.GRB.MAXIMIZE)
 
-    return x, z, m
+    return x, z, h, a, m
 
-def saveResults(m, x, z, params):
+def saveResults(m, x, z, h, a, params):
     outputPath = f'output/{params.instance}_{params.modelType}'
     m.write(f'{outputPath}/hc.lp')
     result = {}
+
     if m.SolCount >= 1:
+        # saving schedule result
         for o in params.operRooms:
             result[o] = {}
             for b in params.blockIds:
@@ -88,6 +94,7 @@ def saveResults(m, x, z, params):
 
         df.to_csv(f'{outputPath}/hc_output.csv')
 
+        # saving count of blocks per specialty
         count_blocks = {}
         for o in params.operRooms:
             for b in params.blockIds:
@@ -100,6 +107,7 @@ def saveResults(m, x, z, params):
         df_count = pd.DataFrame(count_blocks.items(), columns=['Specialty', 'Number of Blocks'])
         df_count.to_csv(f'{outputPath}/hc_output_count.csv') 
 
+        # saving deficit per specialty
         count_deficit = {}
         for s in params.specialties:
             count_deficit[s] = z[s].x
@@ -107,19 +115,35 @@ def saveResults(m, x, z, params):
         df_deficit = pd.DataFrame(count_deficit.items(), columns=['Specialty', 'Deficit'])
         df_deficit.to_csv(f'{outputPath}/hc_output_deficit.csv')   
 
+        # saving teams availability per block
+        teams = {}
+        for b in params.blockIds:
+            teams[b] = {}
+            for s in params.specialties:
+                teams[b][s] = int(h[b, s].x)
+        df_teams = pd.DataFrame(teams)
+        df_teams.transpose().to_csv(f'{outputPath}/hc_output_teams.csv')
+
+        # saving anesthetists availability per block
+        anest = {}
+        for b in params.blockIds:
+            anest[b] = int(a[b].x)
+        df_anest = pd.DataFrame(anest.items(), columns=['Block', 'Anesthetists'])
+        df_anest.to_csv(f'{outputPath}/hc_output_anest.csv')
+
     else:
         print('No solution found')
 
 def main():
     rd.seed(123)
     
-    instance = 'INST_3'
+    instance = 'INST_3_1'
     modelType = 'M1'
     modelParams = getParameters(instance, modelType)
-    alocVars, deficitVars, model = createModel(modelParams)
+    alocVars, deficitVars, teamVars, anesthesistsVar, model = createModel(modelParams)
     model.optimize()
 
-    saveResults(model, alocVars, deficitVars, modelParams)
+    saveResults(model, alocVars, deficitVars, teamVars, anesthesistsVar, modelParams)
 
 if __name__ == '__main__':
     main()
